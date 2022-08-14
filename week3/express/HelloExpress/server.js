fb109e8f21ebac51ea9264719cf05040c95ddc53
top1million <aasahashish@gmail.com>
2022-08-14 16:38:42 +0300

    hello

---
+++ week3/express/HelloExpress/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+app.use( express.json() );
+app.use( express.urlencoded({ extended: true }) );
+
+// req is short for request
+// res is short for response
+app.get("/api", (req, res) => {
+  res.send("sur express api server is now sending this over to the browser");
+});
+
+const server = app.listen(8000, () =>
+  console.log(`Server is locked and loaded on port ${server.address().port}!`)
+);
+
+
+const users = [
+  { firstName: "Amro",  lastName: "Abuhashish"    },
+  { firstName: "Yara", lastName: "Abuhashish"   },
+];
+  
+app.get("/api/users", (req, res) => {
+  res.json( users );
+});
+
+app.post("/api/users", (req, res) => {
+  // req.body will contain the form data from Postman or from React
+  console.log(req.body);
+  // we can push it into the users array for now...
+  // later on this will be inserted into a database
+  users.push(req.body);
+  // we always need to respond with something
+  res.json( { status: "ok" } );
+});
